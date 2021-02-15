4887c92949ff553de1b3f673673d619a3131c040
VaidaEG <grausliene.vaida@gmail.com>
2021-02-15 15:52:04 +0200

    Strings

---
+++ js/sandbox.js
@@ -0,0 +1,18 @@
+let email = 'mario@thenetninja.co.uk';
+let index = email.indexOf('@');
+let result1 = email.toUpperCase();
+let result2 = email.toLowerCase();
+let result3 = email.lastIndexOf('n');
+let result4 = email.slice(0, 5);
+let result5 = email.substr(4, 10);
+let result6 = email.replace('m', 'w');
+let result7 = email.replace('n', 'w');
+
+console.log(index);
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
+console.log(result6);
+console.log(result7);
