b02af79ddff532711c773c84f01617d17a674398
VaidaEG <grausliene.vaida@gmail.com>
2021-02-15 16:49:15 +0200

    Numbers, concatenation and templates

--- js/sandbox.js
+++ js/sandbox.js
@@ -16,3 +16,50 @@ console.log(result4);
 console.log(result5);
 console.log(result6);
 console.log(result7);
+
+let radius = 10;
+const pi = 3.14;
+console.log(radius, pi);
+
+// math operators +, -, *, /, **, %
+// order of operation - B I D M A S
+let result8 = 10 / 2;
+let result9= radius % 3;
+let result10 = pi * radius**2;
+let result11 = 5 * (10 - 3)**2;
+let likes = 10;
+likes++;
+likes--;
+likes += 10;
+likes -= 10;
+likes *= 2;
+likes /= 2;
+
+console.log(result8);
+console.log(result9);
+console.log(result10);
+console.log(result11);
+console.log(likes);
+
+// Nan - not a number
+console.log(5 / 'hello');
+
+let result12 = 'the blog has ' + likes + ' likes';
+console.log(result12);
+
+const title = 'Best reads of 2019';
+const author = 'Mario';
+const likes2 = 30;
+// concatenation way
+let result13 = 'The blog called ' + title + ' by ' + author + ' has ' + likes2 + ' likes';
+console.log(result13);
+// template string way
+let result14 = `The blog called ${title} by ${author} has ${likes2} likes`;
+console.log(result14);
+// creating html templates
+let html = `
+    <h2>${title}</h2>
+    <p>By ${author}</p>
+    <span>This blog has ${likes2} likes</span>
+`;
+console.log(html);
